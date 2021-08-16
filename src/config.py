375bd0a73a9eba4a4d0da2dd306e9926f3b92e85
Leticia Olábarri <leticiaolabarri@gmail.com>
2021-08-16 12:36:22 +0200

    Upssss :shit:

--- src/config.py
+++ src/config.py
@@ -7,5 +7,3 @@ import os
 #dotenv.load_dotenv()
 
 #POSTGRES = os.getenv("POSTGRES")
-
-POSTGRES = "postgresql://xfflroqovktzrq:867ea0218c9bd5defc221488492d8b2124256957e96ef9e3d53a27d3068c658c@ec2-54-74-60-70.eu-west-1.compute.amazonaws.com:5432/da248eh6i18kfp"
