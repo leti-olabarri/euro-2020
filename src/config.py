2896713bd46dd2eff0e1ffefbb2ad2805e04eb17
Leticia Olábarri <leticiaolabarri@gmail.com>
2021-08-16 12:30:21 +0200

    Flask api is now dockerized

--- src/config.py
+++ src/config.py
@@ -1,7 +1,11 @@
 import os
-import dotenv
 
-dotenv.load_dotenv()
+# Uncomment all these to use it in your localhost
 
-PORT = int(os.getenv("PORT"))
-POSTGRES = os.getenv("POSTGRES")
+#import dotenv
+
+#dotenv.load_dotenv()
+
+#POSTGRES = os.getenv("POSTGRES")
+
+POSTGRES = "postgresql://xfflroqovktzrq:867ea0218c9bd5defc221488492d8b2124256957e96ef9e3d53a27d3068c658c@ec2-54-74-60-70.eu-west-1.compute.amazonaws.com:5432/da248eh6i18kfp"
